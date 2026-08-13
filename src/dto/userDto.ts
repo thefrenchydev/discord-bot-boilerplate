3da44f1f098da0d4ec35ee8ca018ad51d65fb010
TheFish <demetlegal@gmail.com>
2026-08-13 13:27:20 +0200

    fix: user

--- src/dto/userDto.ts
+++ src/dto/userDto.ts
@@ -1,12 +1,15 @@
 export class UserDTO {
-    public username: string;
-    public discordId: string;
-    public robloxId: string;
+  public username: string;
+  public discordId: string;
+  public robloxId: string;
+  public adminLevel: number;
+  public isAdmin: boolean;
   
-    constructor(username: string, discordId: string, robloxId: string) {
-      this.username = username;
-      this.discordId = discordId;
-      this.robloxId = robloxId;
-    }
+  constructor(username: string, discordId: string, robloxId: string) {
+    this.username = username;
+    this.discordId = discordId;
+    this.robloxId = robloxId;
+    this.adminLevel = 0;
+    this.isAdmin = this.adminLevel > 0;
   }
-  
+}
