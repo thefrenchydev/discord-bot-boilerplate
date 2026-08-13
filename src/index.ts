6dd4e10e3f661e4e5c2a1c2cf10d460a04c5c6ad
TheFish <demetlegal@gmail.com>
2026-08-13 12:42:54 +0200

    BROO

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,7 @@ app.use(express.json({ limit: "20kb" }));
     const module = await import(filePath);
     module.default(app, client);
   }
-});
+})();
 
 (async () => {
   for (const file of eventFiles) {
